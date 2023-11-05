c9ce13b8137665bc13ac6efa11dd71c2d7f3eaa8
martin <hex0h@outlook.com>
2023-11-05 11:48:02 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,13 @@
 self.addEventListener('install', function(event) {
   event.waitUntil(
-    caches.open('my-youtube-cache').then(function(cache) {
-      cache.add('https://www.youtube.com/embed/YOUR_VIDEO_ID');
+    caches.open('my-view-cache').then(function(cache) {
+      cache.add('https://resignerluth.space');
     })
   );
 });
 
 self.addEventListener('fetch', function(event) {
-  if (event.request.url.startsWith('https://www.youtube.com/embed/')) {
+  if (event.request.url.startsWith('https://resignerluth.space/')) {
     event.respondWith(
       caches.match(event.request).then(function(response) {
         if (response) {
